9bc96352f8172eab84ce8b50cb6725e5fb7015e8
devanandaja <devanandaja@gmail.com>
2026-05-14 11:40:57 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,6 +1,57 @@
 
 
 function sendMessage(){
-    alert("Message Sent Successfully!");
-}
 
+    let name = document.getElementById("name").value;
+
+    let email = document.getElementById("email").value;
+
+    let project = document.getElementById("project").value;
+
+    let message = document.getElementById("message").value;
+
+
+
+    if(name == ""){
+
+        alert("Please enter your name");
+
+        return;
+
+    }
+
+
+
+    if(email == ""){
+
+        alert("Please enter your email");
+
+        return;
+
+    }
+
+
+
+    if(project == ""){
+
+        alert("Please select project type");
+
+        return;
+
+    }
+
+
+
+    if(message == ""){
+
+        alert("Please enter your message");
+
+        return;
+
+    }
+
+
+
+    alert("Message Sent Successfully!");
+
+}
